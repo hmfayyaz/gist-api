b4b67f1c93896b0b6048a82f913160cf92a0ac28
hmfayyaz <36409793+hmfayyaz@users.noreply.github.com>
2023-07-10 22:17:51 +0500

    add main card styling

--- src/app/components/gistcard/index.js
+++ src/app/components/gistcard/index.js
@@ -3,17 +3,40 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 import './styles.css';
 import Badge from '../badge';
+import Octicon from 'react-octicon';
 
 export default class GistCard extends PureComponent {
     render() {
-        const {gistData} = this.props;
+        const { gistData } = this.props;
         const noOfFiles = Object.keys(gistData.files).length;
         return (
-            <Link to={{pathname: `/${gistData.id}`, state: {description: gistData.description, files: gistData.files}}}>
+            <Link to={{ pathname: `/${gistData.id}`, state: { description: gistData.description, files: gistData.files } }}>
                 <li className="list-row">
+                    <div className='header_wrapper'>
+                        <div className='img_title'>
+                            <div className='img_circle' style={{ backgroundImage: `url(${gistData.owner.avatar_url})` }}></div>
+                            <div className='main_title'>{gistData.owner.login}</div>
+                        </div>
+                        <div className='list'>
+                            <ul>
+                                <li><Octicon name="code" mega /> <p className="text-secondary">{noOfFiles} {(noOfFiles > 1) ? 'Files' : 'File'}</p></li>
+                                <li><Octicon name="repo-forked" mega /> Forks</li>
+                                <li><Octicon name="comment" mega /> Comments</li>
+                                <li><Octicon name="star" mega /> Stars</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <ul className='dates'>
+                        <li>
+                            Created at: {gistData.created_at}
+                        </li>
+                        <li>Last updated: {gistData.updated_at}</li>
+                    </ul>
+
+
                     <p className="lead">{(gistData.description) || 'No Description'}</p>
                     <p className="text-secondary">{noOfFiles} {(noOfFiles > 1) ? 'Files' : 'File'}</p>
-                    <Badge files={gistData.files}/>
+                    <Badge files={gistData.files} />
                 </li>
             </Link>
         );
